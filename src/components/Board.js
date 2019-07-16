dc0def32dd549bad555d3067b47aae59769baee8
Alexey Ivasyuta <ivasyutaalexey@gmail.com>
2019-07-16 19:04:48 +0300

    tic-toc-toe works

--- src/components/Board.js
+++ src/components/Board.js
@@ -3,33 +3,18 @@ import Square from './Square';
 
 export default class Board extends React.Component {
 
-  state = {
-    squares: Array(9).fill(null),
-  };
-
-  handleClickSquare = id => () => {
-    const squares = this.state.squares.slice();
-    squares[id] = 'X';
-    this.setState({
-      squares,
-    })
-  };
-
   renderSquare = i => {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={this.handleClickSquare(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   };
 
   render() {
-    const status = 'Next player: X';
-
     return (
       <div>
-        <div className="status">{status}</div>
         <div className="board-row">
           {this.renderSquare(0)}
           {this.renderSquare(1)}
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,16 +1,12 @@
 import React from 'react';
 
-export default class extends React.Component {
-
-  render() {
-    const {value, onClick} = this.props;
-    return (
-      <button
-        className="square"
-        onClick={onClick}
-      >
-        {value}
-      </button>
-    );
-  }
+export default ({value, onClick}) => {
+  return (
+    <button
+      className="square"
+      onClick={onClick}
+    >
+      {value}
+    </button>
+  );
 }
